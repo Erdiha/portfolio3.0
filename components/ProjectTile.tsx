287599c0fc07976cd577d20b9a5c94e29b5c8546
your.user.name <erdiha@hotmail.com>
2023-09-17 12:51:22 -0700

    fix sizing issue

--- components/ProjectTile.tsx
+++ components/ProjectTile.tsx
@@ -5,27 +5,25 @@ import Image from 'next/image';
 const Card = ({ link, code, title, description, imageUrl }: any) => {
  
   return (
-    <div className='flex flex-col justify-center  md:m-8 md:my-16 my-8 '>
-      <div className=' group md:h-[30rem] md:w-[40rem] [perspective:1000px] w-96 h-[20rem] border'>
+    <div className='flex flex-col justify-center  p-2 m-2 md:m-8 my-0 py-5'>
+      <div className=' group md:h-[25rem] md:w-[35rem] [perspective:1000px] max-w-96 h-[20rem] border'>
         <div className='relative h-full w-full rounded-xl shadow-xl [transform-style:preserve-3d] transition-all duration-500 group-hover:[transform:rotateY(180deg)]'>
           <div className=' absolute inset-0 [backface-visibility:hidden] '>
-            <h1 className='flex w-full text-xl p-2 bg-white/60 backdrop-blur shadow-lg leading-loose tracking-wider uppercase'>
+            <h1 className='flex w-full text-fit p-2 bg-white/60 backdrop-blur shadow-lg leading-loose tracking-wider uppercase'>
               {title}
             </h1>
 
-            <div className={`border flex  w-full h-full border-white absolute  rounded p-[2px] justify-center items-center ${title==='Glazed & Confused'? 'bg-pink-400 pt-2 mt-6':''}`}>
+            <div className={`border flex  w-full h-full border-white absolute  rounded p-[2px] justify-center items-center ${title==='Glazed & Confused'? 'bg-pink-400 pt-2 mt-3 h-[94%]':''}`}>
               <Image
                 src={imageUrl}
                 alt={title}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-
-                style={{ objectFit: 'contain',justifyContent:'center',alignItems:'center',padding:title==='Glazed & Confused'?'5rem':'0px' }}
+               fill
+                style={{ objectFit: 'contain',justifyContent:'center',alignItems:'center', padding:title==='Glazed & Confused'?'2rem ':'0px' }}
               />
             </div>
           </div>
-          <div className='absolute inset-0 text-center [transform:rotateY(180deg)] justify-center flex items-center flex-col md:gap-20 gap-10 md:text-xl rounded-xl tracking-wide leading-loose bg-white/40 px-12 text-neutral-800  [backface-visibility:hidden]'>
-            <p>{description}</p>
+          <div className=' [transform:rotateY(180deg)] justify-evenly w-full h-full flex items-center flex-col md:text-xl rounded-xl tracking-wide leading-loose bg-white/40 px-12 text-neutral-800   [backface-visibility:hidden]'>
+            <p className='text-sm'>{description}</p>
 
             <Link href={code} target='_blank' rel='noreferrer'>
               <Button
